b49b66d92ca3df282b89915f23a71261055f31f6
lichunbin814 <lichunbin814@gmail.com>
2024-06-16 12:56:24 +0800

    send Daily FNG Result via sendgrid mail

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const sgMail = require('@sendgrid/mail');
 const { crawl } = require('./crawler');
 
 const targetUrl = 'aHR0cHM6Ly9wcm9kdWN0aW9uLmRhdGF2aXouY25uLmlvL2luZGV4L2ZlYXJhbmRncmVlZC9ncmFwaGRhdGE=';
@@ -8,20 +9,48 @@ const BgRed = "\x1b[41m";
 const BgGreen = "\x1b[42m";
 
 (async () => {
-
+    const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+    const EMAIL_TO = process.env.EMAIL_TO;
+    const SENDGRID_FROM = process.env.SENDGRID_FROM;
     const FNG_MIN = parseFloat(process.env.FNG_MIN || -1);
     const FNG_MAX = parseFloat(process.env.FNG_MAX || -1);
 
+    if (!SENDGRID_API_KEY) {
+        console.log(`${BgRed}%s${Reset}`, "Please set SENDGRID_API_KEY");
+        process.exit(1);
+    }
+
+    if (!SENDGRID_FROM) {
+        console.log(`${BgRed}%s${Reset}`, "Please set SENDGRID_FROM");
+        process.exit(1);
+    }
+
+    if (!EMAIL_TO) {
+        console.log(`${BgRed}%s${Reset}`, "Please set EMAIL_TO");
+        process.exit(1);
+    }
 
     if (FNG_MAX < 0 || FNG_MIN < 0) {
         console.log(`${BgRed}%s${Reset}`, "Please set FNG_MIN and FNG_MAX");
         process.exit(1);
     }
+
+    sgMail.setApiKey(SENDGRID_API_KEY);
+
     const result = await crawl(Buffer.from(targetUrl, 'base64').toString('utf-8'));
-    console.log("FNG for toady is: ", result);
+    console.log("FNG for today is: ", result);
+
+    const msg = {
+        to: EMAIL_TO,
+        from: SENDGRID_FROM,
+        subject: 'Daily FNG Result',
+        text: `FNG for today is: ${result}`,
+    };
+
     if (result <= FNG_MIN || result >= FNG_MAX) {
         console.log(`${BgRed}%s${Reset}`, "FNG is out of range, pls check your stock account.");
         process.exit(1);
     }
+
     process.exit(0);
 })();
